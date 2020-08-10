c2a75bf7f512daed0a73b327b0b7ca28655a47fc
Evelyn Beltrán Espinoza <67941068+Evelyn-B-E@users.noreply.github.com>
2020-08-09 23:38:00 -0500

    Add files via upload

---
+++ EmpresaSueldo/src/Gerente.java
@@ -0,0 +1,13 @@
+
+public class Gerente extends SupervisorTecnico {
+private double viaticos;
+	
+	public Gerente(String identificacion, double salario, int anhosTrabajados, double viaticos) {
+		super(identificacion, salario, anhosTrabajados);
+		this.viaticos = viaticos;
+	}
+	
+	public double getSalario() {
+		return super.getSalario() + viaticos;
+	}
+}
